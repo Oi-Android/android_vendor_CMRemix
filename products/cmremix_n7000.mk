0ed8e2cb4e35b6c9d91b55cca45efe57291de1da
Readone !! ErcOne <android-armv7a-belalang-tempur@users.noreply.github.com>
2015-06-26 04:48:09 +0700

    add support for n7000

--- products/cmremix_n7000.mk
+++ products/cmremix_n7000.mk
@@ -5,7 +5,7 @@ ifeq (cmremix_n7000,$(TARGET_PRODUCT))
 CMREMIX_BOOTANIMATION_NAME := 800
 
 # Include CM-Remix common configuration
-include vendor/cmremix/config/cmremix_n7000.mk
+include vendor/cmremix/config/cmremix_common.mk
 
 # Inherit CM device configuration
 $(call inherit-product, device/samsung/n7000/cm.mk)
